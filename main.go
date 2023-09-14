8cc0239fd548a9a5cdb5a9b27fb0803f982dcad3
Emond Papegaaij <emond.papegaaij@topicus.nl>
2023-09-14 21:48:37 +0200

    Split template file

--- main.go
+++ main.go
@@ -17,6 +17,27 @@ import (
 //go:embed templates/*
 var tmpls embed.FS
 
+func merge(file string, t *template.Template, model map[string]apimodel.RestType) {
+	f, err := os.Create("./" + file + ".go")
+	if err != nil {
+		log.Fatalf("cannot create %s: %s", file, err)
+	}
+
+	var buf bytes.Buffer
+	err = t.ExecuteTemplate(&buf, file+".go.tmpl", model)
+	if err != nil {
+		log.Fatalf("Template %s failed: %s", file, err)
+	}
+
+	p, err := format.Source(buf.Bytes())
+	if err != nil {
+		log.Printf("Format %s failed: %s", file, err)
+		p = buf.Bytes()
+	}
+	f.Write(p)
+	f.Close()
+}
+
 func main() {
 	ctx := context.Background()
 	loader := &openapi3.Loader{Context: ctx, IsExternalRefsAllowed: false}
@@ -40,23 +61,17 @@ func main() {
 		log.Fatalf("Template parsing failed: %s", err)
 	}
 
-	f, err := os.Create("./impl.go")
-	if err != nil {
-		log.Fatalf("cannot create file: %s", err)
-	}
-
-	var buf bytes.Buffer
-	err = t.ExecuteTemplate(&buf, "full.go.tmpl", model)
-	if err != nil {
-		log.Fatalf("Template failed: %s", err)
-	}
-
-	p, err := format.Source(buf.Bytes())
-	if err != nil {
-		log.Printf("Format failed: %s", err)
-		p = buf.Bytes()
-	}
-	f.Write(p)
-
-	f.Close()
+	merge("full-data-struct-ds", t, model)
+	merge("full-data-struct-rs", t, model)
+	merge("full-helpers", t, model)
+	merge("full-object-attrs-ds", t, model)
+	merge("full-object-attrs-rs", t, model)
+	merge("full-schema-ds", t, model)
+	merge("full-schema-rs", t, model)
+	merge("full-tf-to-data-struct-ds", t, model)
+	merge("full-tf-to-data-struct-rs", t, model)
+	merge("full-tf-to-tkh-ds", t, model)
+	merge("full-tf-to-tkh-rs", t, model)
+	merge("full-tkh-to-tf-ds", t, model)
+	merge("full-tkh-to-tf-rs", t, model)
 }
