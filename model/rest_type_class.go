33e684cd86099c3dcf2b9d2eca928b3f046bc731
Emond Papegaaij <emond.papegaaij@topicus.nl>
2026-03-20 10:00:29 +0100

    TKH-3664 prevent possible index out of bounds error

--- model/rest_type_class.go
+++ model/rest_type_class.go
@@ -77,9 +77,9 @@ func (t *restClassType) IsListOfFindByUuid() bool {
 		strings.HasSuffix(t.APITypeName(), "LinkableWrapperWithCount") {
 		nestedProps := t.AllProperties()
 		var itemsPropertyType RestPropertyType
-		if nestedProps[0].TFName() == "items" {
+		if len(nestedProps) > 0 && nestedProps[0].TFName() == "items" {
 			itemsPropertyType = nestedProps[0].Type.(*restArrayType).itemType
-		} else if nestedProps[1].TFName() == "items" {
+		} else if len(nestedProps) > 1 && nestedProps[1].TFName() == "items" {
 			itemsPropertyType = nestedProps[1].Type.(*restArrayType).itemType
 		}
 		_, ok := itemsPropertyType.(*restFindByUUIDObjectType)
